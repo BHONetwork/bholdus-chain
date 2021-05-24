91f1694ec9725edd6aed3e2f0fdfb3d95391a8aa
Danny Lam <lhdung258@gmail.com>
2021-05-24 17:46:37 +0700

    Add some useful constants

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -42,6 +42,10 @@ pub use sp_runtime::{Perbill, Permill};
 /// Import node primitives
 pub use node_primitives::*;
 
+/// Import some useful constants
+pub mod constants;
+use constants::{currency::*, time::*};
+
 /// Import the template pallet.
 pub use pallet_template;
 
@@ -85,23 +89,6 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
 };
-
-/// This determines the average expected block time that we are targeting.
-/// Blocks will be produced at a minimum duration defined by `SLOT_DURATION`.
-/// `SLOT_DURATION` is picked up by `pallet_timestamp` which is in turn picked
-/// up by `pallet_aura` to implement `fn slot_duration()`.
-///
-/// Change this to adjust the block time.
-pub const MILLISECS_PER_BLOCK: u64 = 6000;
-
-// NOTE: Currently it is not possible to change the slot duration after the chain has started.
-//       Attempting to do so will brick block production.
-pub const SLOT_DURATION: u64 = MILLISECS_PER_BLOCK;
-
-// Time is measured by number of blocks.
-pub const MINUTES: BlockNumber = 60_000 / (MILLISECS_PER_BLOCK as BlockNumber);
-pub const HOURS: BlockNumber = MINUTES * 60;
-pub const DAYS: BlockNumber = HOURS * 24;
 
 /// The version information used to identify this runtime when compiled natively.
 #[cfg(feature = "std")]
