03060c3beed4cf1f7a697bace580d6947a11a951
Nhi Tran <nhi.tran@hawking.network>
2021-08-10 22:51:15 +0700

    add package bholdus_support

--- pallets/bholdus-chainbridge-transfer/src/lib.rs
+++ pallets/bholdus-chainbridge-transfer/src/lib.rs
@@ -3,6 +3,8 @@
 /// Edit this file to define custom logic or remove it if it is not needed.
 /// Learn more about FRAME and the core library of Substrate FRAME pallets:
 /// <https://substrate.dev/docs/en/knowledgebase/runtime/frame>
+use bholdus_support::{MultiCurrency, MultiCurrencyExtended};
+
 pub use pallet::*;
 
 #[cfg(test)]
