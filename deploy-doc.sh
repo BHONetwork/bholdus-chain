ba6531695b411ab459a01b2b6035c5008be144ad
XRP <spemxrp@XRPs-MacBook-Pro.local>
2021-11-27 07:52:35 +0800

    add invalidation id

--- deploy-doc.sh
+++ deploy-doc.sh
@@ -1,3 +1,7 @@
 cargo doc --target-dir ./target/docs
 aws s3 sync ./target/docs/ ${DEPLOY_S3_BUCKET}
 aws cloudfront --region ${AWS_REGION} create-invalidation --distribution-id ${CF_DISTRIBUTION_ID} --paths "/*" "/"
+
+echo "S3 upload success!!!, trigger cache invalidation..."
+invalidation_id=$(aws cloudfront --region ${AWS_REGION} create-invalidation --distribution-id ${CF_DISTRIBUTION_ID} --query 'Location.Invalidation.Id' --output text --paths "/*" "/")
+aws cloudfront --region ${AWS_REGION} wait invalidation-completed --distribution-id ${CF_DISTRIBUTION_ID} --id ${invalidation_id}
