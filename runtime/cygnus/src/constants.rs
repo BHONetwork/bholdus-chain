cfccb237c9c85d785339d1f0b03aac8e82911dbb
Daniel Lam <dpryde@DPRYDE.localdomain>
2021-11-30 13:22:03 +0700

    update weight to fee for cygnus

--- runtime/cygnus/src/constants.rs
+++ runtime/cygnus/src/constants.rs
@@ -64,3 +64,70 @@ pub mod time {
     pub const HOURS: BlockNumber = MINUTES * 60;
     pub const DAYS: BlockNumber = HOURS * 24;
 }
+
+pub mod fee {
+    use bholdus_primitives::Balance;
+    use frame_support::weights::{
+        constants::ExtrinsicBaseWeight, WeightToFeeCoefficient, WeightToFeeCoefficients,
+        WeightToFeePolynomial,
+    };
+    use smallvec::smallvec;
+    use sp_runtime::Perbill;
+    /// Handles converting a weight scalar to a fee value, based on the scale and granularity of the
+    /// node's balance type.
+    ///
+    /// This should typically create a mapping between the following ranges:
+    ///   - [0, `MAXIMUM_BLOCK_WEIGHT`]
+    ///   - [Balance::min, Balance::max]
+    ///
+    /// Yet, it can be used for any other sort of change to weight-fee. Some examples being:
+    ///   - Setting it to `0` will essentially disable the weight fee.
+    ///   - Setting it to `1` will cause the literal `#[weight = x]` values to be charged.
+    pub struct WeightToFee;
+    impl WeightToFeePolynomial for WeightToFee {
+        type Balance = Balance;
+        fn polynomial() -> WeightToFeeCoefficients<Self::Balance> {
+            // in Polkadot, extrinsic base weight (smallest non-zero weight) is mapped to 1/10 CENT:
+            let p = super::currency::UNITS / 1000;
+            let q = Balance::from(ExtrinsicBaseWeight::get());
+            smallvec![WeightToFeeCoefficient {
+                degree: 1,
+                negative: false,
+                coeff_frac: Perbill::from_rational(p % q, q),
+                coeff_integer: p / q,
+            }]
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{
+        currency::{CENTS, DOLLARS, MILLICENTS},
+        fee::WeightToFee,
+    };
+    use crate::MAXIMUM_BLOCK_WEIGHT;
+    use frame_support::weights::{constants::ExtrinsicBaseWeight, WeightToFeePolynomial};
+
+    #[test]
+    // This function tests that the fee for `MAXIMUM_BLOCK_WEIGHT` of weight is correct
+    fn full_block_fee_is_correct() {
+        // A full block should cost 16 DOLLARS
+        println!("Base: {}", ExtrinsicBaseWeight::get());
+        let x = WeightToFee::calc(&MAXIMUM_BLOCK_WEIGHT);
+        println!("Weight fee: {}", x);
+        let y = 8 * DOLLARS;
+        assert!(x.max(y) - x.min(y) < super::currency::UNITS / 1000);
+    }
+
+    #[test]
+    // This function tests that the fee for `ExtrinsicBaseWeight` of weight is correct
+    fn extrinsic_base_fee_is_correct() {
+        // `ExtrinsicBaseWeight` should cost 1/10 of a CENT
+        println!("Base: {}", ExtrinsicBaseWeight::get());
+        let x = WeightToFee::calc(&ExtrinsicBaseWeight::get());
+        println!("Weight fee: {}", x);
+        let y = super::currency::UNITS / 1000;
+        assert!(x.max(y) - x.min(y) < super::currency::UNITS / 1000);
+    }
+}
