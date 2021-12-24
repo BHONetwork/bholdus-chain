5bc435e1e612e93a408bf1e118da443c21d680b7
bholdus-quoc-pham <quoc.pham@bholdus.com>
2021-12-24 13:06:07 +0700

    install pallet-evm

--- runtime/phoenix/src/lib.rs
+++ runtime/phoenix/src/lib.rs
@@ -12,13 +12,13 @@ pub use frame_support::{
     construct_runtime, parameter_types,
     traits::{
         Currency, EqualPrivilegeOnly, Everything, Imbalance, InstanceFilter, KeyOwnerProofSystem,
-        Nothing, OnUnbalanced, Randomness, U128CurrencyToVote,
+        Nothing, OnUnbalanced, Randomness, U128CurrencyToVote, FindAuthor,
     },
     weights::{
         constants::{BlockExecutionWeight, ExtrinsicBaseWeight, RocksDbWeight, WEIGHT_PER_SECOND},
         DispatchClass, IdentityFee, Weight,
     },
-    PalletId, RuntimeDebug, StorageValue,
+    PalletId, RuntimeDebug, StorageValue, ConsensusEngineId,
 };
 use frame_system::{
     limits::{BlockLength, BlockWeights},
@@ -32,13 +32,18 @@ use pallet_session::historical as pallet_session_historical;
 pub use pallet_staking::StakerStatus;
 pub use pallet_timestamp::Call as TimestampCall;
 use pallet_transaction_payment::{CurrencyAdapter, Multiplier, TargetedFeeAdjustment};
+use pallet_evm::{EnsureAddressRoot, EnsureAddressNever, HashedAddressMapping, SubstrateBlockHashMapping};
+// pub use this so we can import it in the chain spec.
+#[cfg(feature = "std")]
+pub use pallet_evm::GenesisAccount;
 use sp_api::impl_runtime_apis;
 use sp_authority_discovery::AuthorityId as AuthorityDiscoveryId;
 use sp_consensus_aura::sr25519::AuthorityId as AuraId;
+use sp_core::crypto::Public;
 use sp_core::{
     crypto::KeyTypeId,
     u32_trait::{_1, _2, _3, _4, _5},
-    OpaqueMetadata, U256,
+    OpaqueMetadata, U256, H160,
 };
 use sp_runtime::curve::PiecewiseLinear;
 use sp_runtime::generic::Era;
@@ -51,7 +56,7 @@ use sp_runtime::{
     transaction_validity::{TransactionPriority, TransactionSource, TransactionValidity},
     ApplyExtrinsicResult, FixedPointNumber, Perbill, Percent, Permill, Perquintill,
 };
-use sp_std::prelude::*;
+use sp_std::{marker::PhantomData, prelude::*};
 
 #[cfg(feature = "std")]
 use sp_version::NativeVersion;
@@ -1095,6 +1100,44 @@ impl pallet_template::Config for Runtime {
     type Event = Event;
 }
 
+pub struct FindAuthorTruncated<F>(PhantomData<F>);
+impl<F: FindAuthor<u32>> FindAuthor<H160> for FindAuthorTruncated<F> {
+	fn find_author<'a, I>(digests: I) -> Option<H160>
+	where
+		I: 'a + IntoIterator<Item = (ConsensusEngineId, &'a [u8])>,
+	{
+		if let Some(author_index) = F::find_author(digests) {
+			let authority_id = Aura::authorities()[author_index as usize].clone();
+			return Some(H160::from_slice(&authority_id.to_raw_vec()[4..24]));
+		}
+		None
+	}
+}
+
+parameter_types! {
+	pub const ChainId: u64 = 0;
+    pub BlockGasLimit: U256 =  U256::from(u32::max_value());
+}
+
+/// Configure the EVM pallet
+impl pallet_evm::Config for Runtime {
+    type FeeCalculator = ();
+	type GasWeightMapping = ();
+	type BlockHashMapping = SubstrateBlockHashMapping<Self>;
+	type CallOrigin = EnsureAddressRoot<AccountId>;
+	type WithdrawOrigin = EnsureAddressNever<AccountId>;
+	type AddressMapping = HashedAddressMapping<BlakeTwo256>;
+	type Currency = Balances;
+	type Event = Event;
+	type Runner = pallet_evm::runner::stack::Runner<Self>;
+	type PrecompilesType = ();
+	type PrecompilesValue = ();
+	type ChainId = ChainId;
+	type BlockGasLimit = BlockGasLimit;
+	type OnChargeTransaction = ();
+	type FindAuthor = FindAuthorTruncated<Aura>;
+}
+
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
     pub enum Runtime where
@@ -1147,6 +1190,8 @@ construct_runtime!(
         BagsList: pallet_bags_list::{Pallet, Call, Storage, Event<T>},
         // Include the custom logic from the pallet-template in the runtime.
         TemplateModule: pallet_template::{Pallet, Call, Storage, Event<T>},
+        // Include the pallet-evm in the runtime.
+		EVM: pallet_evm::{Pallet, Config, Call, Storage, Event<T>},
     }
 );
 
