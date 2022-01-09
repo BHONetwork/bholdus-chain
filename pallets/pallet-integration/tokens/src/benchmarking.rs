5100d2b9385624a65dd6b7ff5729f6510d15a16d
Vu Hoang <hatakebee.konoha@gmail.com>
2022-01-09 11:42:10 +0700

    integration-tokens test benchmark

--- pallets/pallet-integration/tokens/src/benchmarking.rs
+++ pallets/pallet-integration/tokens/src/benchmarking.rs
@@ -1,13 +1,10 @@
-//! Benchmarking setup for integration-tokens
+//! Benchmarking setup for pallet-template
 
 use super::*;
 
 #[allow(unused)]
-use crate::Pallet as IntegrationTokens;
-use frame_benchmarking::{
-    account, benchmarks_instance_pallet, impl_benchmark_test_suite, whitelist_account,
-    whitelisted_caller, benchmarks
-};
+use crate::Pallet as Template;
+use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller, account};
 use frame_system::RawOrigin as SystemOrigin;
 use pallet_contracts::chain_extension::UncheckedFrom;
 use bholdus_primitives::{Balance, TokenId as CurrencyId};
@@ -15,18 +12,22 @@ use bholdus_primitives::{Balance, TokenId as CurrencyId};
 const SEED: u32 = 0;
 
 benchmarks! {
+    where_clause {
+        where
+        T::AccountId: UncheckedFrom<T::Hash>,
+        T::AccountId: AsRef<[u8]>,
+     }
      transfer {
         let amount = Balance::from(100u32);
         let transfer_amount = Balance::from(90u32);
 
-        let currency_id = CurrencyId::from(1);
+        let currency_id = 1;
         let caller: T::AccountId = whitelisted_caller();
         let caller_lookup = T::Lookup::unlookup(caller.clone());
-        
-        let target: T::AccountId = account("target", 0, SEED);
+        let target: T::AccountId = whitelisted_caller();
         let target_lookup = T::Lookup::unlookup(target.clone());
 
-    }: _(SystemOrigin::Signed(caller.clone()), currency_id, target_lookup, transfer_amount)
+    }: _(SystemOrigin::Signed(caller.clone()), target_lookup, currency_id, transfer_amount)
 }
 
-impl_benchmark_test_suite!(IntegrationTokens, crate::mock::new_test_ext(), crate::mock::Test);
+impl_benchmark_test_suite!(Template, crate::mock::new_test_ext(), crate::mock::Test);
