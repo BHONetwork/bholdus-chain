afd756260c15f8880d87adc37014550b24c7e958
Daniel Lam <lhdung258@gmail.com>
2022-05-28 21:42:57 +0700

    refactor(manual-seal): remove manual seal features from phoenix, ulas

--- runtime/phoenix/src/pallets/authorship.rs
+++ runtime/phoenix/src/pallets/authorship.rs
@@ -2,28 +2,13 @@
 
 use crate::*;
 
-#[cfg(not(feature = "manual-seal"))]
 parameter_types! {
 	pub const UncleGenerations: BlockNumber = 5;
 }
 
-#[cfg(feature = "manual-seal")]
-parameter_types! {
-	pub const UncleGenerations: BlockNumber = 0;
-}
-
-#[cfg(not(feature = "manual-seal"))]
 impl pallet_authorship::Config for Runtime {
 	type FindAuthor = pallet_session::FindAccountFromAuthorIndex<Self, Aura>;
 	type UncleGenerations = UncleGenerations;
 	type FilterUncle = ();
 	type EventHandler = (Staking, ImOnline);
-}
-
-#[cfg(feature = "manual-seal")]
-impl pallet_authorship::Config for Runtime {
-	type FindAuthor = ();
-	type UncleGenerations = UncleGenerations;
-	type FilterUncle = ();
-	type EventHandler = ();
 }
--- runtime/phoenix/src/pallets/timestamp.rs
+++ runtime/phoenix/src/pallets/timestamp.rs
@@ -2,23 +2,14 @@
 
 use crate::*;
 
-#[cfg(not(feature = "manual-seal"))]
 parameter_types! {
 	pub const MinimumPeriod: Moment = SLOT_DURATION / 2;
-}
-
-#[cfg(feature = "manual-seal")]
-parameter_types! {
-	pub const MinimumPeriod: Moment = 5;
 }
 
 impl pallet_timestamp::Config for Runtime {
 	/// A timestamp: milliseconds since the unix epoch.
 	type Moment = Moment;
-	#[cfg(not(feature = "manual-seal"))]
 	type OnTimestampSet = Aura;
-	#[cfg(feature = "manual-seal")]
-	type OnTimestampSet = ();
 	type MinimumPeriod = MinimumPeriod;
 	type WeightInfo = pallet_timestamp::weights::SubstrateWeight<Runtime>;
 }
