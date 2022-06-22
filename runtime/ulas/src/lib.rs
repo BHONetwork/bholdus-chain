b931fa8bb2f850f4132b40e6fb72e567bc97f113
Daniel Lam <47636806+dpryde258@users.noreply.github.com>
2022-06-22 11:01:45 +0700

    update ss58 prefix for ulas

--- runtime/ulas/src/lib.rs
+++ runtime/ulas/src/lib.rs
@@ -82,7 +82,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	//   `spec_version`, and `authoring_version` are the same between Wasm and native.
 	// This value is set to 100 to notify Polkadot-JS App (https://polkadot.js.org/apps) to use
 	//   the compatible custom types.
-	spec_version: 1_000_020,
+	spec_version: 1_000_021,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
@@ -97,7 +97,7 @@ pub fn native_version() -> NativeVersion {
 
 parameter_types! {
 	pub const Version: RuntimeVersion = VERSION;
-	pub const SS58Prefix: u16 = 2209;
+	pub const SS58Prefix: u16 = 2207;
 }
 
 parameter_types! {
