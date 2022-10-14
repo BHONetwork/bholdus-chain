86bd00091b9cbb241e3e92aad57847a01e0127bb
Daniel Lâm <daniellam@Daniels-MacBook-Pro.local>
2022-10-14 13:47:24 +0700

    hotfix(ulas): update bonding duration to 1

--- runtime/ulas/src/pallets/staking.rs
+++ runtime/ulas/src/pallets/staking.rs
@@ -23,7 +23,7 @@ pallet_staking_reward_curve::build! {
 
 parameter_types! {
 	pub const SessionsPerEra: sp_staking::SessionIndex = 6;
-	pub const BondingDuration: sp_staking::EraIndex = 24 * 28;
+	pub const BondingDuration: sp_staking::EraIndex = 1;
 	pub const SlashDeferDuration: sp_staking::EraIndex = 24 * 7; // 1/4 the bonding duration.
 	pub const RewardCurve: &'static PiecewiseLinear<'static> = &REWARD_CURVE;
 	pub const MaxNominatorRewardedPerValidator: u32 = 256;
